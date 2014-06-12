2ab74a5659a4940a0bcd2ee1df7f01b9c91a79e5
Javier Palacios <jpalacios@tcpsi.es>
2014-06-12 13:42:03 +0200

    Add configuration parameters to connect to SDC

--- manifests/compute/smartdc.pp
+++ manifests/compute/smartdc.pp
@@ -1,8 +1,14 @@
-
 class nova::compute::smartdc {
 
   nova_config {
     'DEFAULT/compute_driver':      value => "smartdc.SmartdcDriver";
+    'smartdc/cloudapi_url':        value => "https://cloudapi.mad.cint.is.hi.inet";
+    'smartdc/cloudapi_username':   value => "is_cloud";
+    'smartdc/cloudapi_password':   value => "$cloudapi_password";
+    'smartdc/api_version':         value => "~6.5";
+    'smartdc/mapi_url':            value => "http://mapi.mad.cint.is.hi.inet";
+    'smartdc/mapi_username':       value => "admin";
+    'smartdc/mapi_password':       value => "$mapi_password";
   }
 
   package { 'openstack-nova-smartos' :
